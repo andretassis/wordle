2157dcaa9d065b889fde73aafb5c4dfa5d809070
Andre Tassis <124065777+andretassis@users.noreply.github.com>
2023-05-01 18:28:58 -0300

    Add files via upload
    Alterações JS

---
+++ random-word.js
@@ -0,0 +1,3 @@
+const words = '[praia, agua, pedra, peixe, tempo, dente, aberto, amargo, anelar, avesso, balada, bravio, cercar, danado, dobrar, escudo, fagote, herdar, jantar, jogado, juntar, lembrar, levado, livro, lontra, mandar, manter, naval, passar, plantar, ganho, mural, malha, gosto, larva, marcha, calma, arroz, branco, manga, poema, louca, lixar, choro, canto]'
+
+const letra = document.querySelector('.button')
